92504bb8599d5b56a44a97621d127a788b498ef1
billeryolo <gangsterboom182@gmail.com>
2023-05-07 17:04:59 +0600

    Clean code and add 5 round game function

--- script.js
+++ script.js
@@ -3,28 +3,28 @@
 /// take two choices and determine a winner. Make it case insensetive
 /// Game function to play 5 rounds
 
-function randInt(){
+function getRandInt(){
     return Math.floor((Math.random())*3)+1;
 }
-function computerSelecting(){
-   choice = randInt() 
-    if (choice == 1){
+function getComputerSelection(){
+   randInt = getRandInt() 
+    if (randInt == 1){
         return "rock"
     }
-    else if (choice == 2){
+    else if (randInt == 2){
         return "paper"
     }
-    else if (choice == 3){
+    else if (randInt == 3){
         return "scissors"
     }
 }
-function playerSelecting(){
+function getPlayerSelection(){
     return prompt("Make a choice")
 }
 
 function playRound(playerSelection, computerSelection) {
-playerSelection = playerSelecting().toLowerCase();
-computerSelection = computerSelecting().toLocaleLowerCase();
+playerSelection = getPlayerSelection().toLowerCase();
+computerSelection = getComputerSelection().toLocaleLowerCase();
 
 
     if (((playerSelection == "rock") && (computerSelection == "scissors")) || ((playerSelection == "scissors") && (computerSelection == "paper")) || ((playerSelection == "paper") && (computerSelection == "rock"))){
@@ -37,7 +37,7 @@ computerSelection = computerSelecting().toLocaleLowerCase();
         return "lost"
     }
   }
-function game(){
+function playGame(){
     let playerScore = 0;
     let computerScore = 0;
     let round = 0;
