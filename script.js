6ddc2304907001e7411946c7f7e522ec3179748e
billeryolo <gangsterboom182@gmail.com>
2023-05-09 18:14:46 +0600

    add simple ui added a ui to play the game directly on the page, rather than on console. Includes score tracker, winner notification and a restart button.

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 /// take two choices and determine a winner. Make it case insensetive
 /// Game function to play 5 rounds
 
+const div = document.querySelector('#score')
+let playerScore = 0;
+let computerScore = 0;
+
+const div1 = document.createElement('div');
+const body = document.body;
+
+
 function getRandInt(){
     return Math.floor((Math.random())*3)+1;
 }
@@ -21,25 +29,49 @@ function getComputerSelection(){
 function getPlayerSelection(){
     return prompt("Make a choice")
 }
+function addRestartButton(){
+    const restart = document.createElement('button');
+    restart.textContent = "Restart";
+    restart.addEventListener('click', () => {
+        playerScore = 0;
+        computerScore = 0;
+        div.textContent = `SCORE: ${playerScore} - ${computerScore}.`;
+        body.removeChild(div1);
+        body.removeChild(restart);
+    })
+    body.appendChild(restart);
+}
 
 function playRound(playerSelection, computerSelection) {
-playerSelection = getPlayerSelection().toLowerCase();
-computerSelection = getComputerSelection().toLocaleLowerCase();
+if((playerScore < 5) && (computerScore < 5)){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerSelection().toLocaleLowerCase();
 
 
-    if (((playerSelection == "rock") && (computerSelection == "scissors")) || ((playerSelection == "scissors") && (computerSelection == "paper")) || ((playerSelection == "paper") && (computerSelection == "rock"))){
-        return "win"
-    }
-    else if (computerSelection == playerSelection){
-        return "tie"
-    }
-    else{
-        return "lost"
-    }
+        if (((playerSelection == "rock") && (computerSelection == "scissors")) || ((playerSelection == "scissors") && (computerSelection == "paper")) || ((playerSelection == "paper") && (computerSelection == "rock"))){
+            ++playerScore;
+            div.textContent = `SCORE: ${playerScore} - ${computerScore}. You won!`   
+            if(playerScore === 5){
+                div1.textContent = "Congratulations, you have won the game!"
+                body.insertBefore(div1, rock)
+                addRestartButton();
+            }
+        }
+        else if (computerSelection == playerSelection){
+            div.textContent = `SCORE: ${playerScore} - ${computerScore}. It's a tie!`   
+        }
+        else{
+            ++computerScore;
+            div.textContent = `SCORE: ${playerScore} - ${computerScore}. You lost.`
+            if(computerScore === 5){
+                    div1.textContent = "You lost the game. :("
+                    body.insertBefore(div1, rock);
+                    addRestartButton();
+            }   
+        }
   }
-function playGame(){
-    let playerScore = 0;
-    let computerScore = 0;
+}
+/* function playGame(){
     let round = 0;
     while(round < 5){
     let playerChoice = playRound()
@@ -62,4 +94,18 @@ function playGame(){
         }
     }
     (playerScore > computerScore) ? console.log(`Congratulations, you have won the game!`) : playerScore < computerScore ? console.log(`You have lost the game!`) : console.log("It's a draw!")
-}
+} */
+const rock = document.querySelector('#rock');
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.addEventListener('click', () => {
+    playRound("rock");
+})
+paper.addEventListener('click', () => {
+    playRound("paper");
+})
+scissors.addEventListener('click', () => {
+    playRound("scissors");
+})
+
